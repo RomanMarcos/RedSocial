30c4771561f5f5bfe4589935b35725a4e9ea82b1
Marcos Roman <marcoss.-08@hotmail.com>
2024-03-26 14:26:44 -0300

    feat(initial commit): project initialization

---
+++ README.md
@@ -0,0 +1,26 @@
+# MERN STACK
+
+Red social: proyecto de curso Udemy
+
+## Pasos para levantar el proyecto
+
+## Clonar proyecto
+
+- git clone git@github.com:RomanMarcos/MERN-STACK.git
+
+## DOCKER (prerequisitos: docker & docker-compose instalados)
+
+- cd MERN-STACK
+- docker-compose up
+
+## Blog API
+
+- cd api
+- npm install
+- npm start
+
+## Blog web app (Vite + React)
+
+- cd blog-webapp
+- npm install
+- npm start
---
+++ api/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+
+// Express server creation
+const app = express();
+
+app.use(cors());
+app.use(express.json()); // Parse all the objects cames from the requests to a JSON if content-type: application/json
+app.use(express.urlencoded({extended: true})); // For requests that have content-type: form-urlencoded
+
+app.listen(3001, () => { 
+    console.log('Server running at port 3001!');
+});
